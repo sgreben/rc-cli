afe857a5a0eb36ce17db3a6d74c8b828cd072065
sergey-grebenshchikov <sergey.grebenshchikov@codecentric.de>
2017-02-06 09:38:53 +0100

    Added Main and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# `rc` - a rule checker
+
+```
+usage: rc [module.yaml]
+ -c,--constraint <arg>   A constraint to check for the given rules.
+ -h,--help               Print this help message.
+ -rs,--rule-set <arg>    Check only the given rule set.
+```
--- src/main/java/io/github/sgreben/rc/cli/Main.java
+++ src/main/java/io/github/sgreben/rc/cli/Main.java
@@ -9,20 +9,43 @@ import io.github.sgreben.rc.SolverException;
 import io.github.sgreben.rc.cli.outputs.ModuleCheckResultOutput;
 import io.github.sgreben.rc.cli.results.ModuleCheckResult;
 import io.github.sgreben.rc.declarations.ModuleDeclaration;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, SolverException {
-        File file = new File(args[0]);
+    private static DefaultParser optionsParser = new DefaultParser();
+    private static Options options = new Options();
+
+    static {
+        options.addOption("c", "constraint", true, "A constraint to check for the given rules.");
+        options.addOption("rs", "rule-set", true, "Check only the given rule set.");
+        options.addOption("h", "help", false, "Print this help message.");
+    }
+
+    private static void printHelp() {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.printHelp("rc [module.yaml]", options);
+    }
+
+    public static void main(String[] args) throws IOException, SolverException, ParseException {
+        CommandLine commandLine = optionsParser.parse(options, args);
+
+        boolean printHelp = commandLine.hasOption("h") || commandLine.getArgs().length < 1;
+        if (printHelp) {
+            printHelp();
+            return;
+        }
+
+        File file = new File(commandLine.getArgs()[0]);
         Context context = new Context();
         ModuleDeclaration moduleDeclaration = ModuleDeclaration.load(file);
         Module module = moduleDeclaration.compile(context);
         ModuleCheckResult checkResult = ModuleChecker.check(module);
         ModuleCheckResultOutput checkResultYaml = checkResult.compile();
         new ObjectMapper(new YAMLFactory())
-            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-            .writeValue(System.out, checkResultYaml);
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .writeValue(System.out, checkResultYaml);
     }
 }
